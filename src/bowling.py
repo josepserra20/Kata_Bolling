6a1bea2f103b6d6dbfe61368f6954fa0ebebfa1c
josepserra20 <jserravives@cifpfbmoll.eu>
2022-01-19 13:01:05 +0100

    refactor: :recycle: refactorizar
    refactorizar nombre de variables

--- src/bowling.py
+++ src/bowling.py
@@ -4,19 +4,17 @@ class Bolos:
     # Global variables
     X = 10
     DASH = 0
-    SLASH = 10 
 
     def __init__(self, puntos):
         self.puntos = list(puntos)
         self.listConverter = Bolos.converter(self)
 
     def spare(self, index):
-            spareScore = (Bolos.SLASH - self.listConverter[index-1]) + self.listConverter[index + 1]
-            return spareScore
+        return (Bolos.X - self.listConverter[index - 1]) + self.listConverter[index + 1]
 
     def strike(self, index):
-        strikeScore = Bolos.SLASH + self.listConverter[index + 1] + self.listConverter[index + 2]
-        return strikeScore
+        return Bolos.X + self.listConverter[index + 1] + self.listConverter[index + 2]
+        
         
     def converter(self):
         converterList = []
@@ -28,37 +26,35 @@ class Bolos:
             if number == 'X':
                 converterList.append(Bolos.X)
             if number == '/':
-                converterList.append(Bolos.SLASH - int((converterList[index - 1])))
+                converterList.append(Bolos.X - int((converterList[index - 1])))
         return converterList
-
-
 
     def score(self):
 
         # variables
         finalScore = 0
-        frame = 0
+        roll = 0
 
         for index, number in enumerate(self.puntos):
-            if frame < 18:
+            if roll < 18:
                 if number.isdigit():
                     finalScore += int(number)
-                    frame += 1
+                    roll += 1
                 if number == '/':
                     finalScore += Bolos.spare(self, index)
-                    frame += 1
+                    roll += 1
                 if number == 'X':
                     finalScore += Bolos.strike(self, index)
-                    frame += 2
+                    roll += 2
                 if number == '-':
-                    frame += 1
+                    roll += 1
             else:
                 if number.isdigit():
                     finalScore += int(number)
                 if number == 'X':
                     finalScore += Bolos.X
                 if number == '/':
-                    finalScore += (Bolos.SLASH - int(self.listConverter[index-1])) 
+                    finalScore += (Bolos.X - int(self.listConverter[index-1])) 
         return finalScore 
 
         
